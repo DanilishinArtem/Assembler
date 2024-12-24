e8e45e77c0cdcf0c1136bfe0dea7a145d981e060
adanilishin <adanilishin@WIN-ARFHPLSLFR9>
2024-12-24 08:09:23 +0300

    boot loader and file system

--- lesson1/JAZZOS/src/bootloader/boot.asm
+++ lesson1/JAZZOS/src/bootloader/boot.asm
@@ -42,7 +42,107 @@ main:
 
     MOV si, os_boot_msg
     CALL print
+
+    ; 4 segments
+    ; reserved segment: 1 sector (bdb_reserved_sectors)
+    ; FAT (file allocation table): 9 * 2 (bdb_sectors_per_fat * bdb_fat_count) = 18 sectors
+    ; Root directory: 
+    ; Data
+    MOV ax, [bdb_sectors_per_fat]
+    MOV bl, [bdb_fat_count]
+    XOR bh, bh
+    MUL bx
+    ADD ax, [bdb_reserved_sectors] ; LBA of the root directory
+    PUSH ax
+    MOV ax, [bdb_dir_entries_count]
+    SHL ax, 5 ; ax *= 32
+    XOR dx, dx
+    DIV word [bdb_bytes_per_sector] ; (32 * num_of_entries) / bytes_per_sector
+    TEST dx, dx
+    JZ rootDirAfter
+    INC ax
+
+rootDirAfter:
+    MOV cl, al
+    POP ax
+    MOV dl, [ebr_drive_number]
+    MOV bx, buffer
+    CALL disk_read
+    XOR bx, bx
+    MOV di, buffer
+
+searchKernel:
+    MOV si, file_kernel_bin
+    MOV cx, 11
+    PUSH di
+    REPE CMPSB ; repeat comparison of bytes between file_kernel_bin and di
+    POP di
+    JE foundKernel
+
+    ADD di, 32
+    INC bx
+    CMP bx, [bdb_dir_entries_count]
+    JL searchKernel
+
+    JMP kernelNotFound
+
+foundKernel:
+    MOV ax, [di+26] ; di - address of the kernel (26 - offset of the first logical cluster field)
+    MOV [kernel_cluster], ax
+    MOV ax, [bdb_reserved_sectors]
+    MOV bx, buffer
+    MOV cl, [bdb_sectors_per_fat]
+    MOV dl, [ebr_drive_number]
+    CALL disk_read
+
+    MOV bx, kernel_load_segment
+    MOV es, bx
+    MOV bx, kernel_load_offset
+
+loadKernelLoop:
+    MOV ax, [kernel_cluster]
+    ADD ax, 31
+    MOV cl, 1
+    MOV dl, [ebr_drive_number]
+    CALL disk_read
+    ADD bx, [bdb_bytes_per_sector]
+    MOV ax, [kernel_cluster] ; (kernel cluster * 3) / 2
+    MOV cx, 3
+    MUL cx
+    MOV cx, 2
+    DIV cx
+    MOV si, buffer
+    ADD si, ax
+    MOV ax, [ds:si]
+    OR dx, dx
+    JZ even
+
+odd:
+    SHR ax, 4
+    JMP nextClusterAfter
+even:
+    AND ax, 0x0FFF
+
+nextClusterAfter:
+    CMP ax, 0x0FF8
+    JAE readFInished
+
+    MOV [kernel_cluster], ax
+    JMP loadKernelLoop
+
+readFInished:
+    MOV dl, [ebr_drive_number]
+    MOV ax, kernel_load_segment
+    MOV ds, ax
+    MOV es, ax
+    JMP kernel_load_segment:kernel_load_offset
     HLT
+
+kernelNotFound:
+    MOV si, msg_kernel_not_find
+    CALL print
+    HLT
+    JMP halt
 
 halt:
     JMP halt
@@ -84,6 +184,41 @@ disk_read:
 
     CALL lba_to_chs
 
+    MOV ah, 02h
+    MOV di, 3 ; counter
+
+retry:
+    STC
+    INT 13h
+    JNC doneRead
+    CALL diskReset
+    DEC di
+    TEST di, di
+    JNZ retry
+
+failDiskRead:
+    MOV si, read_failure
+    CALL print
+    HLT
+    JMP halt
+
+diskReset:
+    PUSHA
+    MOV ah, 0
+    STC
+    INT 13h
+    JC failDiskRead
+    POPA
+    RET
+
+doneRead:
+    POP di
+    POP dx
+    POP cx
+    POP bx
+    POP ax
+    RET
+
 print:
     PUSH si
     PUSH ax
@@ -107,6 +242,15 @@ done_print:
     RET
 
 os_boot_msg: DB 'Our OS has booted!', 0x0D, 0x0A, 0
+read_failure DB 'Failed to read disk', 0x0D, 0x0A, 0
+file_kernel_bin DB 'KERNEL BIN'
+msg_kernel_not_find DB 'KERNEL.BIN not found!'
+kernel_cluster DW 0
+
+kernel_load_segment EQU 0x2000
+kernel_load_offset EQU 0
 
 TIMES 510-($-$$) DB 0
 DW 0AA55h
+
+buffer: 
